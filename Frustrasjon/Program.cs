1cda895270b54c1d6544baaf304be8a98244a0c6
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-05-13 10:11:21 +0200

    Add project files.

---
+++ Frustrasjon/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+
+namespace yepp {
+
+internal class frustrasjon {
+
+static bool santUsant = true;
+
+static bool isTrue() {
+    return santUsant;
+}
+
+static int[] arr = new int[10];
+
+enum ukedager {
+    mandag,
+    tirsdag,
+    onsdag,
+    torsdag,
+    fredag,
+    lørdag,
+    søndag,
+}
+
+static int n(){
+    return 5;
+}
+
+static void Main(string[] args){
+var values = new int[10];
+values = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    bool ja = isTrue();
+    int num = n();
+    var line = Console.ReadLine();
+    Console.WriteLine(@$"{ja} 
+    {ukedager.fredag} 
+    {line}");
+}
+
+
+}
+}
